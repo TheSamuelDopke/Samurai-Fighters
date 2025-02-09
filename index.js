3df556c202213d125c4172fcd8a48a245cbe595b
TheSamuelDopke <157317232+TheSamuelDopke@users.noreply.github.com>
2025-02-09 13:47:05 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,560 @@
+const canvas = document.querySelector('canvas')
+const ctx = canvas.getContext('2d')
+
+var endGame
+
+var playerAttack
+
+var started = false
+
+var timeoutStarted = false
+
+function limitDigits(input) {
+    
+    if (input.value.length > 3) {
+      input.value = input.value.slice(0, 3); 
+    }
+  }
+
+const startGame = document.querySelector('.startGame')
+const menu = document.querySelector('.menu')
+var resultElement = document.querySelector('.result')
+
+
+var settingsButton = document.querySelector('.settingsButton')
+var menuConfigsBackButton = document.querySelector('.menuConfigs-back')
+var menuConfigs = document.querySelector('.menuConfigs')
+var menuStart = document.querySelector('.menuStart')
+
+
+startGame.addEventListener('click', () =>{
+    if(birds.velocity.y == 0 && birds.position.y == 160 && birds.velocity.x == 0 && birds.position.x == 750 && leftright == undefined){
+            moveBird()
+    }
+
+    
+
+    if(menu.style.display !== 'none'){
+        menu.style.display = 'none'
+    }
+
+    resultElement.style.opacity = '1'
+    resultElement.style.display = 'none'
+    timeoutStarted = false
+
+    if(started !== true){
+        restartGame()
+    }
+    started = true
+    timeOver = false
+    
+})
+
+settingsButton.addEventListener('click', () =>{
+    if(menuStart.style.display !== 'none'){
+        menuStart.style.display = 'none'
+        menuConfigs.style.display = 'flex'
+    }
+})
+
+menuConfigsBackButton.addEventListener('click', () =>{
+    if(menuConfigs.style.display !== 'none'){
+        menuStart.style.display = 'flex'
+        menuConfigs.style.display = 'none'
+    }
+})
+
+var damage = 20
+
+var menuConfigsApply = document.querySelector('.menuConfigs-apply')
+var inputTimer = document.querySelector('.inputTimer')
+var inputDamage = document.querySelector('.inputDamage')
+
+
+
+function attTimer(){
+
+   
+    timer = parseInt(inputTimer.value)
+   
+   document.querySelector('.timer').innerHTML = timer
+}
+
+
+menuConfigsApply.addEventListener('click', () =>{
+
+    attTimer()
+    if(inputDamage.value > 100){
+        damage = 100
+    }else{
+        damage = inputDamage.value
+    }
+    
+    if(menuConfigs.style.display !== 'none'){
+        menuStart.style.display = 'flex'
+        menuConfigs.style.display = 'none'
+    }
+
+})
+
+
+var spanTimer = document.querySelector('.spanTimer')
+var spanDamage = document.querySelector('.spanDamage')
+
+inputTimer.addEventListener('input', () =>{
+    spanTimer.innerHTML = inputTimer.value
+})
+
+inputDamage.addEventListener('input', () =>{
+    spanDamage.innerHTML = inputDamage.value
+})
+
+
+function callofDetermine(){
+    if(!timeoutStarted){
+        timeoutStarted = true
+
+                endGame = setTimeout(() => {      
+                resultElement.style.transition = 'opacity 2s ease';
+                resultElement.style.opacity = '0';
+
+                if(resultElement.style.opacity == '0'){
+                    setTimeout(() =>{
+                        if(menu.style.display !== 'flex'){
+                            menu.style.display = 'flex'
+                        }
+                        resultElement.style.display = 'none'
+                        attTimer()
+                        
+                    }, 2000)
+                    
+            }
+
+    
+}, 3000);}}
+
+
+function restartGame(){
+    player.position.x = 50
+    enemy.position.x = canvas.width - 80
+    player.dead = false
+    enemy.dead = false
+    document.querySelector('.player-health').style.width = '100%'
+    document.querySelector('.player2-health').style.width = '100%'
+    player.restart()
+    enemy.restart()
+    player.health = 100
+    enemy.health = 100
+    
+
+    timerId = setTimeout(decreaseTimer, 1000)
+}
+
+ 
+canvas.width = 1024
+canvas.height = 576
+
+ctx.fillStyle = 'black'
+ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+const gravity = 0.7
+
+const velocity = 0
+
+const background = new Sprite({
+    position: {
+        x: 0,
+        y: 0
+    },
+    imageSrc: './img/background.png',
+    spriteHeight: 1
+
+})
+
+const shop = new Sprite({
+    position: {
+        x: 600,
+        y: 97
+    },
+    imageSrc: './img/shop.png',
+    scale: 3,
+    framesMax: 6,
+    framesHold: 30,
+    spriteHeight: 1
+})
+
+
+
+const player = new Fighter({
+    position: {
+        x: 50,
+        y: 0
+    },
+    velocity: {
+        x: velocity,
+        y: velocity
+    },
+    imageSrc: './img/samuraiMack/idle.png',
+    framesMax: 8,
+    scale: 2.3,
+    offset: {
+        x: 216, //x 216, y 137
+        y: 137
+    },
+    framesHold: 8,
+    attackBox: {
+        offset:{
+            x: 30,
+            y: -10
+        },
+        width: 190,
+        height: 130
+    },
+    //Changes
+    sprites: {
+        idle: {
+            imageSrc: './img/samuraimack/idle.png',
+            framesMax: 8,
+        },
+        run: {
+            imageSrc: './img/samuraimack/run.png',
+            framesMax: 8,
+        },
+        jump: {
+            imageSrc: './img/samuraimack/jump.png',
+            framesMax: 2,
+        },
+        fall: {
+            imageSrc: './img/samuraimack/fall.png',
+            framesMax: 2,
+        },
+        attack1: {
+            imageSrc: './img/samuraimack/attack1.png',
+            framesMax: 6,
+        },
+        takeHit:{
+            imageSrc: './img/samuraimack/takehitwhite.png',
+            framesMax: 4,
+        },
+        death:{
+            imageSrc: './img/samuraimack/death.png',
+            framesMax: 6,
+        }
+
+    }
+})
+
+const enemy = new Fighter({
+    position: {
+        x: canvas.width - 80,
+        y: 0
+    },
+    velocity: {
+        x: velocity,
+        y: velocity
+    },
+    imageSrc: './img/kenji/idle.png',
+    framesMax: 4,
+    scale: 2.3,
+    offset: {
+        x: 216, //   x: 216,y: 147
+        y: 147
+    },
+    framesHold: 10,
+    attackBox: {
+        offset:{
+            x: -175,
+            y: 5
+        },
+        width: 175,
+        height: 140
+    },
+    
+    sprites: {
+        idle: {
+            imageSrc: './img/kenji/idle.png',
+            framesMax: 4,
+        },
+        run: {
+            imageSrc: './img/kenji/run.png',
+            framesMax: 8,
+        },
+        jump: {
+            imageSrc: './img/kenji/jump.png',
+            framesMax: 2,
+        },
+        fall: {
+            imageSrc: './img/kenji/fall.png',
+            framesMax: 2,
+        },
+        attack1: {
+            imageSrc: './img/kenji/attack1.png',
+            framesMax: 4,
+        },
+        takeHit:{
+            imageSrc: './img/kenji/takehit.png',
+            framesMax: 3,
+        },
+        death:{
+            imageSrc: './img/kenji/death.png',
+            framesMax: 7,
+        }
+    }
+
+
+})
+
+const keys = {
+    a: {
+        pressed: false
+    },
+
+    d: {
+        pressed: false
+    },
+
+    arrowright:{
+        pressed: false
+    },
+
+    arrowleft: {
+        pressed: false
+    }
+}
+
+//Is in utils.js
+//decreaseTimer()
+var timeOver = false
+
+
+function animate(){
+    window.requestAnimationFrame(animate)
+    ctx.fillStyle = 'black'
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+    background.update()
+    shop.update()
+    birds.update()
+    ctx.fillStyle = 'rgba(255, 255, 255, 0.05)'
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+    enemy.update()
+    player.update()
+    
+
+
+    //Player 1 movement
+    player.velocity.x = 0
+    
+    
+    if(keys.a.pressed && player.lastKey == 'a'){
+        player.velocity.x = -5
+        player.switchSprite('run')
+    }else if(keys.d.pressed && player.lastKey == 'd'){
+        player.velocity.x = 5
+        player.switchSprite('run')
+    }else{
+        player.switchSprite('idle')
+ 
+    }
+
+    if(player.velocity.y == 0){
+        player.jumping = 0
+    }else if(player.velocity.y < 0){
+        player.switchSprite('jump')
+    }else if(player.velocity.y > 3.5 ){
+        player.switchSprite('fall')
+    }
+
+    //Player 2 movement
+    enemy.velocity.x = 0
+    if(keys.arrowright.pressed && enemy.lastKey == 'arrowright'){
+        enemy.velocity.x = 5
+        enemy.switchSprite('run')
+    }else if(keys.arrowleft.pressed && enemy.lastKey == 'arrowleft'){
+        enemy.velocity.x = -5
+        enemy.switchSprite('run')
+    }else{
+        enemy.switchSprite('idle')
+
+    }
+
+    if(enemy.velocity.y == 0){
+        enemy.jumping = 0
+    }else if(enemy.velocity.y < 0){
+        enemy.switchSprite('jump')
+    }else if(enemy.velocity.y > 3.5 ){
+        enemy.switchSprite('fall')
+    }
+
+
+     //---Detect for collision
+     //Player 1
+     if(rectangularCollision({
+        rectangle1: player, rectangle2: enemy}) && 
+        player.isAttacking && player.framesCurrent == 4) {
+
+        if(player.health <= 0){return}else{
+
+
+            enemy.takeHit()
+            player.isAttacking = false
+
+            if(enemy.health <= 0){
+                gsap.to('.player2-health', {
+                    width: '0%',
+                    duration: 0.5
+                })
+            }
+
+
+            //Using gsap to animate the health bar
+            //document.querySelector('.player2-health').style.width = enemy.health + '%'
+            gsap.to('.player2-health', {
+                width: enemy.health + '%'
+            })
+
+        }
+     }else if(player.isAttacking && player.framesCurrent == 4){
+        player.isAttacking = false
+     }
+
+     
+
+     //Player 2
+     if(rectangularCollision
+    ({rectangle1: enemy, rectangle2: player}) && 
+     enemy.isAttacking && 
+     enemy.framesCurrent == 2) {
+
+        if(enemy.health <= 0){
+            
+            return}
+        
+        else{
+
+
+
+            player.takeHit()
+
+            
+            
+            enemy.isAttacking = false
+
+            if(player.health <= 0){
+                gsap.to('.player-health', {
+                    width: '0%',
+                    duration: 0.5
+                })
+            }
+     
+            //Using gsap to animate the health bar
+            //document.querySelector('.player-health').style.width = player.health + '%'
+            gsap.to('.player-health', {
+                width: player.health + '%',
+                duration: 0.5
+            })
+
+            
+
+        }
+
+     }else if(enemy.isAttacking && enemy.framesCurrent == 2){
+   
+        enemy.isAttacking = false
+
+     }
+
+     if(enemy.health <= 0 || player.health <= 0){
+        determineWinner({player, enemy, timerId})
+    }
+ 
+
+
+    console.log(player.health, enemy.health)
+    
+}
+animate()
+
+
+
+//Keys control
+
+window.addEventListener('keydown', (event) =>{
+    const key = event.key.toLowerCase()
+
+    if(player.health > 0 && !timeOver && started){
+    switch (key){
+        case 'd': 
+            keys.d.pressed = true
+            player.lastKey = 'd'
+            break
+
+        case 'a':
+            keys.a.pressed = true
+            player.lastKey = 'a'
+            break
+
+        case 'w':
+            if(player.jumping < 2){
+            player.jumping++
+            player.velocity.y = -15
+        }
+            break
+
+        case ' ':
+            if(player.isAttacking == false){
+                player.attack()
+            }
+            break
+
+
+    }}
+
+    if(enemy.health > 0 && !timeOver && started){
+    switch(key){
+        case 'arrowright':
+            keys.arrowright.pressed = true
+            enemy.lastKey = 'arrowright'
+            break
+
+        case 'arrowleft':
+            keys.arrowleft.pressed = true
+            enemy.lastKey = 'arrowleft'
+            break
+
+        case 'arrowup':
+            if(enemy.jumping < 2){
+                enemy.jumping++
+                enemy.velocity.y = -15
+            }
+            break
+
+        case 'enter':
+                if(enemy.isAttacking == false){
+                    enemy.attack()
+                }
+            break
+        }}
+})
+
+window.addEventListener('keyup', (event) =>{
+    const key = event.key.toLowerCase()
+
+
+    switch (key){
+        case 'd':
+            keys.d.pressed = false
+            break
+
+        case 'a':
+            keys.a.pressed = false
+            break
+
+        case 'arrowright':
+            keys.arrowright.pressed = false
+            break
+    
+        case 'arrowleft':
+            keys.arrowleft.pressed = false
+            break
+    }
+})
---
+++ js/birds.js
@@ -0,0 +1,316 @@
+const birds = new Sprite({
+    position: {
+        x: 750,
+        y: 160 //160
+    },
+
+    imageSrc: '../img/birds.png',
+    framesMax: 12,
+    scale: 2,
+    spriteHeight: 2,
+    velocity: {
+        x: 0,
+        y: 0
+    },
+    height: 192 / 4,
+    width: 144 / 3,
+    framesHold: 15
+})
+
+var random
+
+var movingBirdTop = false
+var movingBirdLeft = false
+var movingBirdRight = false
+
+var laps = 0
+var birdLaps
+var intervalTimerTopBottom
+var timerintervalMoveBirdTopBottom
+
+var randomY
+
+var birdUp = 1
+
+var velX
+
+var velY
+
+var leftright
+
+var updown
+
+var randomDistanceX
+
+let stopRandom = randomTimer()
+
+var birdsMovingTop = false
+
+function randomDistancePositionX(){
+    randomDistanceX = Math.floor(Math.random()*400) + 100
+}
+
+function randomTimerTopBottom(){
+    intervalTimerTopBottom = Math.floor(Math.random()*900) + 100
+}
+
+function timerMoveBirdTopBottom(){
+    timerintervalMoveBirdTopBottom = Math.floor(Math.random()*2000) + 1000
+}
+
+function randomBirdLaps(){
+    birdLaps = Math.floor(Math.random()*2) + 1
+}
+
+function randomTimer(){
+    random = Math.floor(Math.random()*12) + 3
+    random *= 1000
+}
+
+function randomDirection(){
+    leftright = Math.floor(Math.random()*2)+1
+    
+
+}
+function randomDirectionUpDown(){
+    updown = Math.floor(Math.random()*2)+1
+}
+
+function randomDistanceY(){
+    randomY = Math.floor(Math.random()*30) + 10
+}
+
+function randomVelocity(){
+    velX = 6 //Math.floor(Math.random()*3) + 3
+    velY = Math.floor(Math.random()*2) + 1
+    
+    
+}
+
+
+var intervalMoveBird = null
+
+var intervalMoveBirdSides = null
+
+var intervalMoveBirdTopBottom = null
+
+
+function moveBird(){
+    randomVelocity()
+
+
+
+        
+        if(birds.position.y + birds.velocity.y >= 50){
+
+            if (!intervalMoveBird) { // Só cria um novo intervalo se não houver um ativo
+                intervalMoveBird = setInterval(() => {
+                    moveBird(); // Chama a função novamente dentro do setInterval
+                }, 100);
+            }
+        
+            
+            birds.velocity.y = -1.5
+            console.log('Execution?')
+            movingBirdTop = true
+            birdUp++
+            
+        }else{
+            birds.velocity.y = 0
+            if (intervalMoveBird) { // Se o intervalo existir, para ele
+                clearInterval(intervalMoveBird);
+                intervalMoveBird = null;
+                console.log('Nada to tipo?');
+            }
+            setTimeout(() => {
+                
+                    intervalMoveBirdSides = setInterval(moveBirdSides, 100)
+                    
+                
+
+                //birds.position.y = 200
+            }, 600)
+            mbsexe1 = 0
+            laps = 0
+            
+        }
+
+
+        
+    }
+
+var mbsexe1 = 0
+function moveBirdSides(){
+    
+    if(mbsexe1 < 1){
+        timerMoveBirdTopBottom()
+        randomDistancePositionX()
+        randomTimerTopBottom()
+        randomDirectionUpDown()
+        randomDirection()
+        randomBirdLaps()
+        randomVelocity()
+        mbsexe1++
+    }
+    console.log(updown)
+    
+   
+    
+    if(mbsexe1 < 2){
+
+    if(leftright == 1){
+ 
+            movingBirdTop = false
+            movingBirdRight = false
+            movingBirdLeft = true
+            birds.velocity.x = -3
+      
+        
+        
+    }else if(leftright == 2){
+        
+            movingBirdTop = false
+            movingBirdRight = true
+            movingBirdLeft = false
+            birds.velocity.x = 3
+         
+    }
+    mbsexe1++
+    }
+    if(birds.position.x == 750 && birds.velocity.x == 0 && birds.position.y == 160 && birds.velocity.y == 0){
+        movingBirdRight = false
+        movingBirdLeft = false
+        birds.framesCurrent = 0
+        clearInterval(intervalMoveBirdSides)
+        intervalMoveBirdSides = null
+        setTimeout(() =>{
+            moveBird()
+        }, random)
+    }
+    console.log('Posições: ' + birds.position.x, birds.position.y)
+    console.log('Velocidades: ' + birds.velocity.x, birds.velocity.y)
+
+    if(birds.position.x + birds.velocity.x <= -60){
+        birds.position.x = 1300
+        laps++
+    }
+
+    if(birds.position.x + birds.velocity.x >= 1400){
+        birds.position.x = -60
+        laps++
+    }
+
+    console.log('Velocidade X: ' + birds.velocity.x)
+    if(laps >= birdLaps){
+        var velPos = 0
+        var dirVelPos = 0
+        var dirVelNeg = 0
+        var velNeg = 0
+        clearInterval(intervalMoveBirdTopBottom)
+        intervalMoveBirdTopBottom = null
+
+        if(birds.velocity.x > 0 && birds.velocity.x + birds.position.x >= 750){
+            birds.velocity.x = 0
+            birds.position.x = 750
+        }
+
+        if(birds.velocity.x < 0 && birds.velocity.x + birds.position.x <=750){
+            birds.velocity.x = 0
+            birds.position.x = 750
+        }
+
+
+        //Positive Direction
+        if(birds.velocity.y + birds.position.y < 160 && birds.velocity.x > 0 && birds.position.x >= randomDistanceX){
+            birds.velocity.y = 2
+           
+            if(birds.position.y + birds.velocity.y >= 160){
+                birds.position.y = 160
+                birds.velocity.y = 0
+            }
+        }else if(birds.position.y + birds.velocity.y >= 160 && birds.position.x >= randomDistanceX && birds.velocity.y > 0){
+            birds.velocity.y = 0
+            birds.position.y = 160
+
+    }
+
+    if(birds.velocity.y + birds.position.y > 160 && birds.velocity.x > 0 && birds.position.x >= randomDistanceX){
+        birds.velocity.y = -2
+        if(birds.position.y + birds.velocity.y <= 160){
+            birds.position.y = 160
+            birds.velocity.y = 0
+        }
+        
+    }else if(birds.position.y + birds.velocity.y <= 160 && birds.position.x >= randomDistanceX && birds.velocity.y <= -1){
+        birds.velocity.y = 0
+        birds.position.y = 160
+
+}
+
+
+
+    //Negative Direction
+    if(birds.velocity.y + birds.position.y < 160 && birds.velocity.x < 0 && birds.position.x <= 950){
+
+            birds.velocity.y = 2
+        
+
+    }else if(birds.position.y + birds.velocity.y >= 160 && birds.position.x <= 950 && birds.velocity.y > 0){
+        birds.velocity.y = 0
+        birds.position.y = 160
+
+}
+
+if(birds.velocity.y + birds.position.y > 160 && birds.velocity.x < 0 && birds.position.x <= 950){
+
+        birds.velocity.y = -2
+
+}else if(birds.position.y + birds.velocity.y <= 160 && birds.position.x <= 950 && birds.velocity.y <= -1){
+    birds.velocity.y = 0
+    birds.position.y = 160
+
+}
+
+
+
+
+
+
+    }else {
+        if (!intervalMoveBirdTopBottom) {
+            intervalMoveBirdTopBottom = setInterval(moveBirdTopBottom, timerintervalMoveBirdTopBottom); // Cria o intervalo apenas se não existir um timerintervalMoveBirdTopBottom
+        }
+        
+    }
+    console.log(timerintervalMoveBirdTopBottom)
+}
+    
+
+
+function moveBirdTopBottom(){
+    randomDirectionUpDown()
+    console.log('EXECUTANDO BIRD TOP BOTTOM')
+   
+    if(updown == 1){
+        if(birds.velocity.y + birds.position.y >= 16){
+            birds.velocity.y = -1
+        }
+            
+        
+        
+    }else if(updown == 2){
+        birds.velocity.y = 1
+    }
+
+    setTimeout(() =>{
+            birds.velocity.y = 0
+            
+    }, intervalTimerTopBottom)
+}
+
+
+
+
+
+
+
